80bcdfb7e7f7cb1bf26475a9ebb2393f821fcc65
Krishna Mahawar <krishnazindahai@gmail.com>
2026-07-11 16:06:06 +0530

    perf: async font loading + route code splitting
    - make google fonts non-render-blocking via media=print trick (~1200ms savings)
    - code-split all routes with React.lazy() (initial JS 438KB -> 175KB gzip)
    - heavy pages (ReferenceChecker, BetaReader) load only when visited

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,37 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Research1 from './pages/Research1';
-import Research2 from './pages/Research2';
-import Research3 from './pages/Research3';
-import Research4 from './pages/Research4';
-import Research5 from './pages/Research5';
-import ResearchMain from './pages/ResearchMain';
-import ResearchTools from './pages/ResearchTools';
-import ReferenceChecker from './pages/ReferenceChecker';
-import BetaReader from './pages/BetaReader';
 import MerchBanner from './components/MerchBanner';
+
+// Lazy-load all non-homepage routes to reduce initial bundle size
+const Research1 = lazy(() => import('./pages/Research1'));
+const Research2 = lazy(() => import('./pages/Research2'));
+const Research3 = lazy(() => import('./pages/Research3'));
+const Research4 = lazy(() => import('./pages/Research4'));
+const Research5 = lazy(() => import('./pages/Research5'));
+const ResearchMain = lazy(() => import('./pages/ResearchMain'));
+const ResearchTools = lazy(() => import('./pages/ResearchTools'));
+const ReferenceChecker = lazy(() => import('./pages/ReferenceChecker'));
+const BetaReader = lazy(() => import('./pages/BetaReader'));
 
 function App() {
   return (
     <BrowserRouter>
       <MerchBanner />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/fellowships" element={<Research1 />} />
-        <Route path="/professor-database" element={<Research2 />} />
-        <Route path="/cold-email-templates" element={<Research3 />} />
-        <Route path="/internship-guide" element={<Research4 />} />
-        <Route path="/project-ideas" element={<Research5 />} />
-        <Route path="/career-roadmaps" element={<ResearchMain />} />
-        <Route path="/research-tools-and-resources" element={<ResearchTools />} />
-        <Route path="/reference-checker" element={<ReferenceChecker />} />
-        <Route path="/beta-reader" element={<BetaReader />} />
-      </Routes>
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/fellowships" element={<Research1 />} />
+          <Route path="/professor-database" element={<Research2 />} />
+          <Route path="/cold-email-templates" element={<Research3 />} />
+          <Route path="/internship-guide" element={<Research4 />} />
+          <Route path="/project-ideas" element={<Research5 />} />
+          <Route path="/career-roadmaps" element={<ResearchMain />} />
+          <Route path="/research-tools-and-resources" element={<ResearchTools />} />
+          <Route path="/reference-checker" element={<ReferenceChecker />} />
+          <Route path="/beta-reader" element={<BetaReader />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
